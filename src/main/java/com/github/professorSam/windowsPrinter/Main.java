1915fb9186e4d14a184fb35eb032b623816287c8
rosenstein <rosenstein@DESKTOP-8CEC3SG.server.lan>
2022-04-06 14:41:22 +0200

    Project setup

---
+++ src/main/java/com/github/professorSam/windowsPrinter/Main.java
@@ -0,0 +1,8 @@
+package com.github.professorSam.windowsPrinter;
+
+public class Main {
+	
+	public static void main(String... args) {
+		
+	}
+}
---
+++ src/main/java/com/github/professorSam/windowsPrinter/Printer.java
@@ -0,0 +1,5 @@
+package com.github.professorSam.windowsPrinter;
+
+public class Printer {
+
+}
